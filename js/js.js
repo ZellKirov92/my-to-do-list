8dfd7b634c5e8bea08fae7fd7723ed717b39906e
ZellKirov92 <gwen.dragonrouge@gmail.com>
2024-02-21 00:00:31 +0100

    modif index add main and div modif link index js and css mofif style.css modif js.js add button and event remove li

--- js/js.js
+++ js/js.js
@@ -12,10 +12,26 @@ addTaskButton.addEventListener('click', () => {
 
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
+        checkbox.style.marginLeft = "30px";
+        checkbox.style.textAlign = "right";
+        checkbox.style.width = "20px";
+        checkbox.style.height = "20px";
         li.appendChild(checkbox);
+        const removeTask = document.createElement("button");
+        removeTask.type = "button";
+        removeTask.style.width = "40px";
+        removeTask.style.height = "40px";
+        removeTask.textContent = "X";
+        removeTask.style.margin = "5px 10px 0 20px";
+        removeTask.style.padding = "5px 5px 10px 5px";
+        removeTask.style.backgroundColor = "hsl(0, 100%, 33%)";
+        removeTask.addEventListener("click", () => {
+        li.remove();
+    });
+        li.appendChild(removeTask);
 
         checkbox.addEventListener('click', function() {
-            li.classList.toogle("completed");
+            li.classList.toggle("completed");
         });
     }
 });
